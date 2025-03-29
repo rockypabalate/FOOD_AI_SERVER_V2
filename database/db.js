54516e8c0c437493b967be67d3f2eab5c02cd50a
Rockz <125625074+rockypabalate@users.noreply.github.com>
2025-03-29 14:42:44 +0800

    live

--- database/db.js
+++ database/db.js
@@ -1,54 +1,23 @@
-// const mysql = require('mysql2');
-// require('dotenv').config();
+const mysql = require('mysql2');
+require('dotenv').config();
 
-// // MySQL connection setup
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'food_ai_database',
-//  // port: 3307 
-// });
-
-// db.connect(err => {
-//   if (err) {
-//     console.error('Database connection failed:', err);
-//   } else {
-//     console.log('Connected to MySQL database');
-//   }
-// });
-
-// module.exports = db;
-
-
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-
-// Database connection options
-const dbOptions = {
-    host: process.env.DB_HOST || 'shinkansen.proxy.rlwy.net',
-    user: process.env.DB_USER || '27700',
-    password: process.env.DB_PASSWORD || 'xyUMsPMhoDeKaWhDYhADqhbMXIpSSMag',
-    database: process.env.DB_NAME || 'railway',
-    port: process.env.DB_PORT || 3306,
-    clearExpired: true, // Automatically removes expired sessions
-    checkExpirationInterval: 900000, // 15 minutes
-    expiration: 86400000, // 24 hours
-};
-
-const sessionStore = new MySQLStore(dbOptions);
-
-const sessionMiddleware = session({
-    key: 'session_id',
-    secret: 'food_ai_database',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false, // Set to true if using HTTPS
-        httpOnly: true,
-        maxAge: 86400000, // 24 hours
-    },
+// MySQL connection setup
+const db = mysql.createConnection({
+  host: '192.185.48.158',
+  user: 'bisublar_foodai',
+  password: 'Fo0d4ai2025',
+  database: 'bisublar_foodai',
+ // port: 3307 
 });
 
-module.exports = sessionMiddleware;
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
+module.exports = db;
+
+
--- middleware/session.middleware.js
+++ middleware/session.middleware.js
@@ -1,11 +1,11 @@
-// //session.middleware.js
-// const session = require('express-session');
+//session.middleware.js
+const session = require('express-session');
 
-// const sessionMiddleware = session({
-//     key: 'session_id',
-//     secret: 'food_ai_database',
-//     resave: false,
-//     saveUninitialized: true,
-// });
+const sessionMiddleware = session({
+    key: 'session_id',
+    secret: 'food_ai_database',
+    resave: false,
+    saveUninitialized: true,
+});
 
-// module.exports = sessionMiddleware;
+module.exports = sessionMiddleware;
